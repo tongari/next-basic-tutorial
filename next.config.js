3c4087d828e072a5ef2c778ce934976575cc228c
tongari <info@tongari-as.com>
2023-01-03 22:13:45 +0900

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,38 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
-module.exports = {
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  webpack(config, options) {
-    config.resolve.alias['@'] = path.join(__dirname, './')
-    return config
+// module.exports = {
+//   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+//   webpack(config, options) {
+//     config.resolve.alias['@'] = path.join(__dirname, './')
+//     return config
+//   },
+// }
+
+
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+
+const config = {
+  resolve: {
+    alias: {
+      '@' : path.join(__dirname, './'),
+    }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public,max-age=300,immutable',
+          },
+        ],
+      },
+    ]
   },
 }
+
+module.exports = config
